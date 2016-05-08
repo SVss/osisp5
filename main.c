7e05eac85c6fa1838ceaf40f28c89e76f18dd23d
Vladislav Savyonok <savionak@gmail.com>
2016-05-08 14:07:13 +0300

    Add print_help function

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 
 #include <unistd.h>
 #include <sys/syscall.h>
+#include <signal.h>
 
 #include <sys/types.h>
 #include <pthread.h>
@@ -29,6 +30,8 @@ proc_args_t **threads_args = NULL; /* array of pointers */
 
 char *exec_name;
 
+void print_help();
+
 void print_error(char *msg);
 void *process(void *args);
 
@@ -41,58 +44,53 @@ int main(int argc, char *argv[])
 
     if (argc < 2) {
         print_error("Not enough arguments");
+        print_help();
         exit(EXIT_FAILURE);
     }
 
     /* process params */
     errno = 0;
 
-    int elm_count_N = strtol(argv[1], NULL, 10);  /* N */
+    int array_size_N = strtol(argv[1], NULL, 10);  /* N */
     int mems_count_n = strtol(argv[2], NULL, 10);  /* n */
 
-    if ( (errno != 0) && !is_valid(elm_count_N) && !is_valid(mems_count_n) ) {
+    if ( (errno != 0) && !is_valid(array_size_N) && !is_valid(mems_count_n) ) {
 
        print_error("Bad arguments!");
+       print_help();
        exit(EXIT_FAILURE);
     }
 
+    /* threads input list */
     threads_args = (proc_args_t**)malloc(sizeof(proc_args_t*) * mems_count_n);
-
-    pthread_t tid = 0;
 
     pthread_attr_t thread_attr;
     pthread_attr_init(&thread_attr);
     pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_DETACHED);
 
-    int i = 0;
-    for (i = 0; i < mems_count_n; ++i) {   /* creating threads */
-        threads_args[i] = ST_THRD_TERMINATE;
+    int i = 0;    
 
-        if (pthread_create(&tid, &thread_attr, &process, (threads_args+i) ) == -1) {
-            print_error("Error creating thread!\n");
-            exit(EXIT_FAILURE);
-        }
+    /* empty threads list */
+    for (i = 0; i < mems_count_n; ++i) {
+        threads_args[i] = ST_THRD_TERMINATE;
     }
 
     int curr_thrd = 0;
 
     int j = 0;
-    for (j = 0; j < elm_count_N; ++j) {
+    for (j = 0; j < array_size_N; ++j) {
         for (i = 0; i < mems_count_n; ++i) {
             proc_args_t* targs = malloc(sizeof(proc_args_t) );
 
             targs->i = i;
-            targs->N = elm_count_N;
+            targs->N = array_size_N;
             targs->member_number = i;
 
             /* wait for any thread to be ready */
             while (threads_args[curr_thrd] != ST_THRD_WAIT) {
-                ++curr_thrd;
+                curr_thrd = (curr_thrd + 1) % mems_count_n ;
 
-                if (curr_thrd == mems_count_n)
-                    curr_thrd = 0;
-
-                printf("%d\n", curr_thrd);
+//                printf("%d\n", curr_thrd);
             }
 
             threads_args[curr_thrd] = targs;
@@ -119,52 +117,83 @@ int main(int argc, char *argv[])
 }   /* main */
 
 
+void print_help() {
+    printf("\n  osisp5 N n\n"
+           "  Count sine function for array of N elements with Taylor series.\n\n"
+           "  Arguments (natural numbers):\n"
+           "  \tN - number of elements in array\n"
+           "  \t\teach element y[i]=sin(2*PI*i/N) ( i=0,1,2…N-1 )\n\n"
+           "  \tn - Taylor series members quantity\n"
+           "  \t\t(the more n is - the better approximation is)\n\n");
+
+}   /* print_help */
+
+
 char is_valid(long val) {
     return !(val == LONG_MAX || val == LONG_MIN || val == 0);
 }   /* is_valid */
 
 
 void print_error(char *msg) {
-    printf("%s: %s\nr", exec_name, msg);
+    printf("%s: %s\n", exec_name, msg);
 
 }   /* print_error */
 
 
-double get_sin_taylor_member(const int i, const int N, const int member_number) {
-    unsigned long long num = 1, den = 1;
-
-    for (int j = 1; j <= (2*member_number + 1); ++j) {
-        num *= 2*M_PI*i/N;
-        den *= j;
-    }
-
-    double result = num / den;
-    return (member_number % 2) ? -result : result;
-
-}   /* get_sin_taylor_member */
-
-
 void print_result(pthread_t id, double result) {
+
     /* ToDo: print result to terminal and file */
-    printf("%lu\t%f\n", id, result);
+
+    printf("%lu %f\n", id, result);
 
 }   /* print_result */
 
 
+double get_sin_taylor_member(double x, int member_number) {
+    double result = 1;
+    for (int i = 1; i <= member_number * 2 + 1; i++) {
+        result *= x / i;
+
+    }
+    return (member_number % 2) ? -result : result;
+}
+
+/* TODO: make with signals */
+/*
+ * sigmask
+ *
+ * tkill
+ *
+ * sigwait
+ *
+ * */
+
+
 void *process(void *args) {
-    pid_t self_id = syscall(SYS_gettid);
+    pid_t tid = syscall(SYS_gettid);
     proc_args_t **args_p = (proc_args_t**)args;
 
     *args_p = ST_THRD_WAIT;
 
+    double result = 0;
+
     do {
         while ( (*args_p) == ST_THRD_WAIT) ;    /* wait for job */
 
-        double result = get_sin_taylor_member( (*args_p)->i, (*args_p)->N, (*args_p)->member_number);
+        double x = (2 * M_PI * (*args_p)->i)/(*args_p)->N;
+        if (x != 0){
+            x = M_PI - x;
+        }
 
-        print_result(self_id, result);
+        result = get_sin_taylor_member(x, (*args_p)->member_number);
+
+        print_result(tid, result);
+
+        (*args_p) = ST_THRD_WAIT;
 
     } while ( (*args_p) != ST_THRD_TERMINATE);
+
+    args_p = NULL;
 
     free(args);
     return NULL;
